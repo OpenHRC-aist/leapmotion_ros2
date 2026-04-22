c1273d6c655f4929509aa228094132e7b640b12a
em-sor <jemilio.sorianocampos@aist.go.jp>
2026-04-22 16:49:10 +0900

    Updated Finger detector
    Changed the tip of the fingers to be relative to
    the wrist frame instead of the world frame.

--- leap_node/leap_node/finger_detector.py
+++ leap_node/leap_node/finger_detector.py
@@ -1,10 +1,12 @@
 import rclpy
 from rclpy.node import Node
-from leap_msgs.msg import Finger  # カスタムメッセージ型をインポート
-from geometry_msgs.msg import Point
+from leap_msgs.msg import Finger # カスタムメッセージ型をインポート
+from geometry_msgs.msg import Point, Vector3
 import leap
 import time
 import curses
+import numpy as np
+from scipy.spatial.transform import Rotation
 
 class FingerDetector(Node):
     '''
@@ -12,37 +14,89 @@ class FingerDetector(Node):
     '''
     def __init__(self, stdscr):
         super().__init__('finger_detector')
-        self.left_hand_publisher = self.create_publisher(Finger, 'left_hand_fingers', 10)
-        self.right_hand_publisher = self.create_publisher(Finger, 'right_hand_fingers', 10)
+        self.left_fingers_publisher = self.create_publisher(Finger, 'left_hand_fingers', 10)
+        self.right_fingers_publisher = self.create_publisher(Finger, 'right_hand_fingers', 10)
         self.stdscr = stdscr
         self.listener = LeapMotionListener(self.handle_finger_data)
         self.connection = leap.Connection()
         self.connection.add_listener(self.listener)
 
-    def _to_meters(self, leap_pos):
-        return Point(
-            x=-leap_pos.z * 0.001,  # Z -> -X 
-            y=-leap_pos.x * 0.001,  # X -> -Y
-            z=leap_pos.y * 0.001    # Y -> Z
-        )
+    def position_relative_to_wrist(self, hand, bone):
+        wrist_position = np.array([hand.arm.next_joint.x, hand.arm.next_joint.y, hand.arm.next_joint.z])
+        q_wrist_position = Rotation.from_quat([hand.palm.orientation.x, hand.palm.orientation.y, hand.palm.orientation.z, hand.palm.orientation.w])
+        tip_position = np.array([bone.next_joint.x, bone.next_joint.y, bone.next_joint.z])
+        tip_relative = q_wrist_position.inv().apply(tip_position - wrist_position)
+        return tip_relative
+
+    def bone_length(self, bone):
+        tip = bone.next_joint
+        base = bone.prev_joint
+        return np.linalg.norm([tip.x - base.x, tip.y - base.y, tip.z - base.z])
+    
+    def finger_length(self, digit):
+        return sum(self.bone_length(digit.bones[i]) for i in range(4))
+
+    def finger_direction(self, bone):
+        q = Rotation.from_quat([bone.rotation.x, bone.rotation.y, bone.rotation.z, bone.rotation.w])
+        direction = q.apply([0, 0, -1])
+        return direction
 
     def handle_finger_data(self, hands):
         # 検出された手を処理
         self.stdscr.clear()
         for hand in hands:
+
             for digit in hand.digits:
-                
                 finger_msg = Finger()
                 finger_msg.id = digit.finger_id
-                tip_point = self._to_meters(digit.distal.next_joint)
-                finger_msg.tip_position = tip_point
-                
+                rel_position = self.position_relative_to_wrist(hand, digit.distal)
+                finger_msg.tip_position.x = -rel_position[2] * 1.0e-3
+                finger_msg.tip_position.y = -rel_position[0] * 1.0e-3
+                finger_msg.tip_position.z = rel_position[1] * 1.0e-3
+                direction = self.finger_direction(digit.distal)
+                finger_msg.tip_direction.x = -direction[2]
+                finger_msg.tip_direction.y = -direction[0]
+                finger_msg.tip_direction.z = direction[1]
+                finger_msg.tip_orientation.x = -digit.distal.rotation.z
+                finger_msg.tip_orientation.y = -digit.distal.rotation.x
+                finger_msg.tip_orientation.z = digit.distal.rotation.y
+                finger_msg.tip_orientation.w = digit.distal.rotation.w
+                finger_msg.length = self.finger_length(digit) * 1.0e-3
+                finger_msg.width = digit.intermediate.width * 1.0e-3
+
                 if str(hand.type) == "HandType.Left":
-                    self.left_hand_publisher.publish(finger_msg)
-                    self.stdscr.addstr(f"Left Hand Finger ID {finger_msg.id}:\n Tip Position: x={finger_msg.tip_position.x:.2f}, y={finger_msg.tip_position.y:.2f}, z={finger_msg.tip_position.z:.2f}\n")
+                    self.left_fingers_publisher.publish(finger_msg)
+                    try:
+                        self.stdscr.addstr(
+                            f"Left Hand Finger ID {finger_msg.id}:\n"
+                            f" Tip Position: x={finger_msg.tip_position.x:.2f},"
+                            f" y={finger_msg.tip_position.y:.2f},"
+                            f" z={finger_msg.tip_position.z:.2f}\n"
+                            f" Tip Direction: x={finger_msg.tip_direction.x:.2f},"
+                            f" y={finger_msg.tip_direction.y:.2f},"
+                            f" z={finger_msg.tip_direction.z:.2f}\n"
+                            f" Length: {finger_msg.length:.2f}\n"
+                            f" Width: {finger_msg.width:.2f}\n"
+                        )
+                    except curses.error:
+                        pass
+                        
                 elif str(hand.type) == "HandType.Right":
-                    self.right_hand_publisher.publish(finger_msg)
-                    self.stdscr.addstr(f"Right Hand Finger ID {finger_msg.id}:\n Tip Position: x={finger_msg.tip_position.x:.2f}, y={finger_msg.tip_position.y:.2f}, z={finger_msg.tip_position.z:.2f}\n")
+                    self.right_fingers_publisher.publish(finger_msg)
+                    try:
+                        self.stdscr.addstr(
+                            f"Right Hand Finger ID {finger_msg.id}:\n"
+                            f" Tip Position: x={finger_msg.tip_position.x:.2f},"
+                            f" y={finger_msg.tip_position.y:.2f},"
+                            f" z={finger_msg.tip_position.z:.2f}\n"
+                            f" Tip Direction: x={finger_msg.tip_direction.x:.2f},"
+                            f" y={finger_msg.tip_direction.y:.2f},"
+                            f" z={finger_msg.tip_direction.z:.2f}\n"
+                            f" Length: {finger_msg.length:.2f}\n"
+                            f" Width: {finger_msg.width:.2f}\n"
+                        )
+                    except curses.error:
+                        pass
         self.stdscr.refresh()
 
     def run(self):
